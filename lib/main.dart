321164168dac391516a0890700592bac9c3539b5
Mohamed Ramadan <mohamedramadan6420@gmail.com>
2023-07-25 13:44:37 +0300

    register adapter and open future box

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:booklyapp/Features/home/domain/entities/book_entity.dart';
 import 'package:booklyapp/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive/hive.dart';
 
 import 'core/utils/const.dart';
 
-void main() {
+void main() async {
   runApp(const Bookly());
+  Hive.registerAdapter(BookEntityAdapter());
+  await Hive.openBox(kFutureBox);
 }
 
 class Bookly extends StatelessWidget {
